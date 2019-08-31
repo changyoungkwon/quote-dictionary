cad41aa1f8a5be34ac4673e6f35bed53c784afea
Changyoung <changyoung.kwon.illugrammer@gmail.com>
2019-08-31 12:33:13 +0900

    test-sets append

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import spacy
+import argparse
 
-nlp = spacy.load('en_core_web_sm')
-doc = nlp('Hello World!')
+def main(content):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(content)
+    #1
+    for chunk in doc.noun_chunks: 
+        print("noun phrase : '{}' with root '{}'".format(chunk.text, chunk.root.text))
+        print("dependency : {}: {}".format(chunk.root.dep_, spacy.explain(chunk.root.dep_)))
+    print("="*10)
+    #2 traverse
+    root = root_token(doc)
+    traverse(root)
+    print("="*10)
 
-print('{0:10} {1:10} {2:10} {3:10} {4:10} {5:10} {6:10} {7:10}'.format('text', 'lemma', 'pos', 'tag', 'dep', 'shape', 'isalpha', 'isstop'))
-for token in doc:
-    print('{0:10} {1:10} {2:10} {3:10} {4:10} {5:10} {6:10} {7:10}'.format(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop))
+    #3 subtree
+    for token in doc[0].subtree:
+        print(token)
+
+def root_token(doc):
+    _token = doc[0]
+    while _token != _token.head:
+        _token = _token.head
+    return _token
+
+def traverse(root_token):
+    """depth first"""
+    for child in root_token.children:
+        print('head  : {}, ({}: {})'.format(root_token, root_token.pos_, root_token.tag_))
+        print('child : {}, ({}: {})'.format(child, child.pos_, child.tag_))
+        print('dependency : {}({})'.format(child.dep_, spacy.explain(child.dep_)))
+        print('-'*10)
+        traverse(child)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='write')
+    parser.add_argument(dest='input', type=str)
+    args = parser.parse_args()
+    print(args.input)
+    main(args.input)
